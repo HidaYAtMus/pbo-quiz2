ac7bb05e081e0e8c89e7e0c63fe88f22e350aa41
HP <HP@169.254.181.209>
2018-11-30 20:34:35 +0700

    menambahkan function

--- mus/quiz2/Transaksi.java
+++ mus/quiz2/Transaksi.java
@@ -28,7 +28,15 @@ public class Transaksi extends javax.swing.JFrame {
     //arraylist untuk menyimbpan transaksi
     private  ArrayList<Item> cart = new ArrayList<>();
     
-    
+    public Transaksi(){
+        ComboModel comboModel = new ComboModel();
+        this.cbModel = new DefaultComboBoxModel<>(comboModel.getNama().toArray()); // Set Combo items
+        
+        TableModel tableModel = new TableModel(); 
+        this.tbModel = new DefaultTableModel(tableModel.getColumnName(), 0); // Set Table Column name
+        
+        initComponents();
+    }
     
     //function setCode
     private String setCode() {
@@ -67,18 +75,7 @@ public class Transaksi extends javax.swing.JFrame {
     }
     
     //update jumlah 
-    private void updateJumlah(String name, int add) {
-        ArrayList<String> item = new ArrayList<>();
-        for (int i = 0; i < tbModel.getRowCount(); i++){
-            item.add(tbModel.getValueAt(i, 0).toString());
-        }
-        for(int i = 0; i < item.size(); i++) {
-            if(item.get(i).equals(name)) {
-                int jumlah = new Integer(tbModel.getValueAt(i, 2).toString());
-                tbModel.setValueAt(jumlah+add, i, 2);  
-            } 
-        }
-    } 
+    
     
     //mengcek apakah ada duplikasi atau tidak
     private boolean isDuplicate(String name) {

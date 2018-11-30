cf83fb6ac190cede3b200b59085b54db5ca5a6fb
HP <HP@169.254.181.209>
2018-11-30 20:27:10 +0700

    menambahkan variable

---
+++ mus/quiz2/Item.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz2;
+
+/**
+ *
+ * @author HP
+ */
+public class Item {
+    //variable dari item nama
+    private String nama;
+    //variable dari item harga
+    private float harga;
+    //variable dari item jumlah
+    private int jumlah;
+    
+   
+}

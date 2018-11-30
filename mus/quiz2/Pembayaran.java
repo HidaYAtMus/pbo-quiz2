44e4b13b06f47cd42e2af8c1346df237f044a40e
HP <HP@169.254.181.209>
2018-11-30 20:29:39 +0700

    menambahkan variable

---
+++ mus/quiz2/Pembayaran.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz2;
+import java.util.*;
+/**
+ *
+ * @author HP
+ */
+public class Pembayaran {
+    //variable code 
+    private final String code;
+    //variable array list
+    private ArrayList<Item> items = new ArrayList<>();
+    //variable total
+    private float total;
+
+}
